fbb4f7350f154156f926d6071a72c0cc503bb3b4
vishnukumar.ps@outlook.com <vishnukumar.ps@outlook.com>
2022-06-01 08:45:54 +0530

    Interface added

---
+++ Services/Interface/ITeacherservice.cs
@@ -0,0 +1,12 @@
+﻿using DemoEmployee.Models;
+
+namespace DemoEmployee.Services.Interface
+{
+    public interface ITeacherservice
+    {
+
+        public Teacher Add(Teacher teacher);
+        public List<Teacher> GetAll();
+        public Teacher Get(Guid id);
+    }
+}
